465fe319db3e4fbd6665afbf95ecd04c510a4f05
wjm0452 <wjm0452@gmail.com>
2023-06-29 22:16:26 +0900

    인증 오류 처리 추가

--- frontend/src/common/httpClient.js
+++ frontend/src/common/httpClient.js
@@ -10,31 +10,74 @@ class HttpClient {
     config.url = this.getApiUrl(config.url);
     return axios({
       ...config,
+    }).catch((e) => {
+      if (e.response.status == 403) {
+        const data = e.response.data;
+        alert(data.message);
+      }
+
+      throw e;
     });
   }
 
   get(url, config = {}) {
-    return axios.get(this.getApiUrl(url), {
-      ...config,
-    });
+    return axios
+      .get(this.getApiUrl(url), {
+        ...config,
+      })
+      .catch((e) => {
+        if (e.response.status == 403) {
+          const data = e.response.data;
+          alert(data.message);
+        }
+
+        throw e;
+      });
   }
 
   delete(url, config = {}) {
-    return axios.get(this.getApiUrl(url), {
-      ...config,
-    });
+    return axios
+      .get(this.getApiUrl(url), {
+        ...config,
+      })
+      .catch((e) => {
+        if (e.response.status == 403) {
+          const data = e.response.data;
+          alert(data.message);
+        }
+
+        throw e;
+      });
   }
 
   post(url, data, config = {}) {
-    return axios.post(this.getApiUrl(url), data, {
-      ...config,
-    });
+    return axios
+      .post(this.getApiUrl(url), data, {
+        ...config,
+      })
+      .catch((e) => {
+        if (e.response.status == 403) {
+          const data = e.response.data;
+          alert(data.message);
+        }
+
+        throw e;
+      });
   }
 
   put(url, data, config = {}) {
-    return axios.put(this.getApiUrl(url), data, {
-      ...config,
-    });
+    return axios
+      .put(this.getApiUrl(url), data, {
+        ...config,
+      })
+      .catch((e) => {
+        if (e.response.status == 403) {
+          const data = e.response.data;
+          alert(data.message);
+        }
+
+        throw e;
+      });
   }
 
   getApiUrl(url) {
