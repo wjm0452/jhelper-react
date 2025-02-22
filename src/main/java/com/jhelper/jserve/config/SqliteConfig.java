c64825ad6ad0794f1ef643d9df7c19182127af7c
wjm0452 <wjm0452@gmail.com>
2025-02-22 19:04:07 +0900

    h2 추가가

--- src/main/java/com/jhelper/jserve/config/SqliteConfig.java
+++ src/main/java/com/jhelper/jserve/config/SqliteConfig.java
@@ -4,13 +4,13 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.hibernate.cfg.AvailableSettings;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
@@ -30,11 +30,14 @@ public class SqliteConfig {
         em.setDataSource(dataSource);
         em.setPackagesToScan(new String[] { "com.jhelper.jserve" });
 
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabasePlatform("com.jhelper.jserve.config.jdbc.SQLiteDialect");
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(true);
         em.setJpaVendorAdapter(vendorAdapter);
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "com.jhelper.jserve.config.jdbc.SQLiteDialect");
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty(AvailableSettings.HBM2DDL_AUTO, "update");
+        properties.setProperty(AvailableSettings.SHOW_SQL, "true");
         em.setJpaProperties(properties);
 
         return em;
