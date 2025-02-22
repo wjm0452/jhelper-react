04ead6a1c5b0ed172af58b32f12fbc04c78dec71
wjm0452 <wjm0452@gmail.com>
2025-02-22 21:55:46 +0900

    검색모드 설정정

--- src/main/java/com/jhelper/jserve/board/BoardConfig.java
+++ src/main/java/com/jhelper/jserve/board/BoardConfig.java
@@ -1,7 +1,9 @@
 package com.jhelper.jserve.board;
 
+import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
@@ -10,11 +12,13 @@ import com.jhelper.jserve.board.repository.BoardRepository;
 import com.jhelper.jserve.board.repository.ElalsticBoardRepository;
 import com.jhelper.jserve.board.service.BoardServiceImpl;
 import com.jhelper.jserve.board.service.ElalsticBoardSerivce;
+import com.jhelper.jserve.board.service.SearchBoardService;
 import com.jhelper.jserve.config.ElasticConfig;
 
 @Configuration
 public class BoardConfig {
 
+    @ConditionalOnProperty(prefix = "jhelper.board", name = "searchMode", havingValue = "search")
     @ConditionalOnBean(ElasticConfig.class)
     @Configuration
     static class ElasticBoardSerivceConfiguration {
@@ -28,21 +32,22 @@ public class BoardConfig {
         }
     }
 
+    @ConditionalOnProperty(prefix = "jhelper.board", name = "searchMode", havingValue = "search")
     @ConditionalOnMissingBean({ ElasticConfig.class })
     @Configuration
     static class BasicboardSerivceConfiguration {
+        @Bean
+        public BoardService boardService(BoardRepository boardRepository, SearchSession searchSession) {
+            return new SearchBoardService(boardRepository, searchSession);
+        }
+    }
+
+    @ConditionalOnProperty(prefix = "jhelper.board", name = "searchMode", havingValue = "db", matchIfMissing = true)
+    @Configuration
+    static class SearchBasicboardSerivceConfiguration {
         @Bean
         public BoardService boardService(BoardRepository boardRepository) {
             return new BoardServiceImpl(boardRepository);
         }
     }
-
-    // @ConditionalOnMissingBean({ ElasticConfig.class })
-    // @Configuration
-    // static class BasicboardSerivceConfiguration {
-    //     @Bean
-    //     public BoardService boardService(BoardRepository boardRepository, SearchSession searchSession) {
-    //         return new SearchBoardService(boardRepository, searchSession);
-    //     }
-    // }
 }
